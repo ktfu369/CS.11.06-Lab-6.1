1ace25fcf6f49dee540a2a3168153458047d3cc8
Katie <kf1030@student.cis.edu.hk>
2023-11-25 19:28:49 +0800

    bruh

--- src/AdventureTime.java
+++ src/AdventureTime.java
@@ -9,8 +9,16 @@ public class AdventureTime {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
+        int challengeOneAnswer = challengeOne("InputOneTwo.txt");
+        System.out.println(challengeOneAnswer);
+        int challengeTwoAnswer = challengeTwo("InputOneTwo.txt");
+        System.out.println(challengeTwoAnswer);
+        int challengeThreeAnswer = challengeThree("InputThreeFour.txt");
+        System.out.println(challengeThreeAnswer);
+        int challengeFourAnswer = challengeFour("InputThreeFour.txt");
+        System.out.println(challengeFourAnswer);
 
-
+        writeFileAllAnswers("AdventureTime.txt",challengeOneAnswer,challengeTwoAnswer,challengeThreeAnswer,challengeFourAnswer);
     }
 
     /** TODO 1
@@ -22,7 +30,22 @@ public class AdventureTime {
      * @throws IOException
      */
     public static int challengeOne(String fileName) throws IOException {
-        return 0;
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        int cnt=0;
+        int last=-1;
+        while(scanner.hasNextLine()){
+            int ok = scanner.nextInt();
+            if(last==-1) {
+
+            }else{
+                if(ok>last) {
+                    cnt++;
+                }
+            }
+            last=ok;
+        }
+        return cnt;
     }
 
     /** TODO 2
@@ -34,7 +57,25 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeTwo(String fileName) throws FileNotFoundException {
-        return 0;
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        int num1=0,num2=0,num3=0;
+        int lastSum=0;
+        int pos=0;
+        int cnt=0;
+        while(scanner.hasNextLine()){
+            num1=num2;
+            num2=num3;
+            num3=scanner.nextInt();
+            if(pos>2){
+                if(num1+num2+num3>lastSum){
+                    cnt++;
+                }
+                lastSum = num1+num2+num3;
+            }
+            pos++;
+        }
+        return cnt;
     }
 
     /** TODO 3
@@ -46,7 +87,25 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeThree(String fileName) throws FileNotFoundException {
-        return 0;
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        int h=0,v=0;
+        while(scanner.hasNextLine()){
+            String ok = scanner.nextLine();
+            String word="";
+            int x=0;
+            for(int i=0;i<ok.length();i++){
+                if(ok.charAt(i)>='0'&&ok.charAt(i)<='9'){
+                    x = x*10 + ok.charAt(i)-'0';
+                }else if(ok.charAt(i)!=' '){
+                    word+=ok.charAt(i);
+                }
+            }
+            if(word.equals("forward")) h+=x;
+            else if(word.equals("up")) v-=x;
+            else if(word.equals("down")) v+=x;
+        }
+        return h*v;
     }
 
     /** TODO 4
@@ -58,7 +117,27 @@ public class AdventureTime {
      * @throws FileNotFoundException
      */
     public static int challengeFour(String filename) throws FileNotFoundException {
-        return 0;
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        int h=0,v=0,aim=0;
+        while(scanner.hasNextLine()){
+            String ok = scanner.nextLine();
+            String word = "";
+            int x = 0;
+            for(int i=0;i<ok.length();i++){
+                if(ok.charAt(i)>='0'&&ok.charAt(i)<='9'){
+                    x = x*10 + ok.charAt(i)-'0';
+                }else if(ok.charAt(i)!=' '){
+                    word += ok.charAt(i);
+                }
+            }
+            if(word.equals("forward")){
+                h+=x;
+                v+=x*aim;
+            }else if(word.equals("down")) aim+=x;
+            else if(word.equals("up")) aim-=x;
+        }
+        return h*v;
     }
 
     /** This method will write the values passed as challengeOne, challengeTwo, challengeThree, and challengeFour to a text file.
